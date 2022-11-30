791ee1cd983d10a486b2d44cd6f9ce12f417a882
Erengun <erengun4@gmail.com>
2022-11-30 16:45:36 +0300

    New assets and slider

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -7,8 +7,14 @@ class Assets {
   // home screen assets
   static const String bannerImage = "assets/images/banner_image.jpeg";
 
+  // home page assets
+  static const String serviceDrawer1 = "assets/images/service_drawer_1.png";
+  static const String serviceDrawer2 = "assets/images/service_drawer_2.png";
+  static const String serviceDrawer3 = "assets/images/service_drawer_3.png";
+
   // Location screen assets
   static const String locationBanner = "assets/images/location_banner.png";
+
   
 
 }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:boilerplate/constants/app_theme.dart';
 import 'package:boilerplate/constants/assets.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -13,25 +15,160 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: _homeAppBar(),
       body: _homePageBody(),
+    );
+  }
+
+  PreferredSizeWidget _homeAppBar() {
+    return AppBar(
+      title: Text("Home"),
+      automaticallyImplyLeading: false,
+      backgroundColor: AppThemeData.lightColorScheme.primary,
+      elevation: 4.0,
     );
   }
 
   Widget _homePageBody() {
     List<Image> imageList = [
-      Image.asset(Assets.bannerImage,),
-      Image.asset(Assets.bannerImage,),
-      Image.asset(Assets.bannerImage,)
+      Image.asset(
+        Assets.bannerImage,
+        fit: BoxFit.fill,
+        width: 2000.0,
+      ),
     ];
+
+    List<Widget> bannerSliders = imageList
+        .map((item) => Container(
+              child: Container(
+                margin: EdgeInsets.all(0.1),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(0.1)),
+                    child: Stack(
+                      children: <Widget>[
+                        item,
+                      ],
+                    )),
+              ),
+            ))
+        .toList();
+
+    List<Widget> drawerList = [
+      Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          Image.asset(
+            Assets.serviceDrawer1,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                ' Hizmet Al',
+                style: GoogleFonts.roboto(color: Colors.white),
+              ),
+              Text(
+                ' Ilanlari',
+                style: GoogleFonts.roboto(color: Colors.white),
+              ),
+            ],
+          ),
+        ],
+      ),
+      Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          Image.asset(
+            Assets.serviceDrawer2,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                ' Hali yikama',
+                style: GoogleFonts.roboto(color: Colors.white),
+              ),
+              Text(
+                ' Ilanlari',
+                style: GoogleFonts.roboto(color: Colors.white),
+              ),
+            ],
+          ),
+        ],
+      ),
+      Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          Image.asset(
+            Assets.serviceDrawer3,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                ' Koltuk Yikama',
+                style: GoogleFonts.roboto(color: Colors.white),
+              ),
+              Text(
+                'Ilanlari',
+                style: GoogleFonts.roboto(color: Colors.white, shadows: [
+                  Shadow(
+                    blurRadius: 100.0,
+                    color: Colors.black,
+                    offset: Offset(1.0, 1.0),
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ];
+
+    List<Widget> imageSliders = drawerList
+        .map((item) => Container(
+              child: Container(
+                margin: EdgeInsets.all(5.0),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    child: Stack(
+                      children: <Widget>[
+                        item,
+                      ],
+                    )),
+              ),
+            ))
+        .toList();
     return SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(height: 90,),
+          Container(
+            height: 30.0,
+            color: AppThemeData.lightColorScheme.primary,
+          ),
+          Container(
+            color: AppThemeData.lightColorScheme.primary,
+            child: CarouselSlider(
+              items: bannerSliders,
+              options: CarouselOptions(
+                autoPlay: true,
+                enlargeCenterPage: true,
+                viewportFraction: 1.0,
+                pauseAutoPlayOnTouch: true,
+              ),
+            ),
+          ),
+          Container(
+            color: AppThemeData.lightColorScheme.primary,
+            height: 50,
+          ),
           CarouselSlider(
-            items: imageList,
+            items: imageSliders,
             options: CarouselOptions(
-              autoPlay: true,
-              enlargeCenterPage: true
+              autoPlay: false,
+              enlargeCenterPage: false,
+              height: 311,
+              viewportFraction: 0.45,
             ),
           ),
         ],
